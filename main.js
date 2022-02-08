e4e3ae87fd95f63b36be8fa4b31c5ac9fe3a5350
nishant-k02 <nishantsk2002@gmailcom>
2022-02-08 13:15:46 +0530

    First Commit

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-alert("Welcome to To Do Web App")
+// JS for input task section
+
+window.addEventListener('load', () => 
+{
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    form.addEventListener('submit', (e) => 
+    {
+        e.preventDefault();
+
+        const task = input.value;
+
+        if (!task)
+        {
+            alert("Atleast add single task for the day!!");
+            return;
+        } 
+
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
+
+        task_el.appendChild(task_content_el);
+
+        list_el.appendChild(task_el);
+        
+    })
+})
